742cd12b3ef34b0479fbd5efc2421d98cb1d26fc
saraUsama9910 <sarausama80@gmail.com>
2024-04-20 00:37:20 +0200

    ADD PADDING TO TEXT FIELD

--- lib/Features/search/peresntation/views/widgets/search_view_body.dart
+++ lib/Features/search/peresntation/views/widgets/search_view_body.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [CustomSearchTextField()],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: [CustomSearchTextField()],
+      ),
     );
   }
 }
@@ -16,6 +20,15 @@ class CustomSearchTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return TextField(
+      decoration: InputDecoration(
+          hintText: 'Search',
+          suffixIcon: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.magnifyingGlass,
+                size: 22,
+              ))),
+    );
   }
 }
