d46efb4da558c3d794c927d87f4b511550a79829
saraUsama9910 <sarausama80@gmail.com>
2024-04-20 17:10:11 +0200

    create failure file

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,8 +1,9 @@
 import 'package:bookapp/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookapp/core/errors/failures.dart';
 import 'package:dart_either/dart_either.dart';
 
 abstract class HomeRepo {
-  Future<Either> featchBestSellerBooks();
-  Future<Either> fetchFeaturedBooks();
-  Future<Either> fetchSimilarBooks();
+  Future<Either<Failure, List<BookModel>>> featchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks();
 }
