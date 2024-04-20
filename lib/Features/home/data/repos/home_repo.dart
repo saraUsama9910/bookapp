70c30793364080123d62479c9dd42cddbc26c2f6
saraUsama9910 <sarausama80@gmail.com>
2024-04-20 15:46:38 +0200

    fetchSimilarBooks add to repo

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -3,4 +3,5 @@ import 'package:bookapp/Features/home/data/models/book_model/book_model.dart';
 abstract class HomeRepo {
   Future<List<BookModel>> featchBestSellerBooks();
   Future<List<BookModel>> fetchFeaturedBooks();
+  Future<List<BookModel>> fetchSimilarBooks();
 }
