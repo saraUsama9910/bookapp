ed84e0b448f1211251306038bbfe8059bcbb6513
saraUsama9910 <sarausama80@gmail.com>
2024-04-21 22:38:32 +0200

    call homerepo in method of cubit

--- lib/Features/home/peresntation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/peresntation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,11 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookapp/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookapp/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
 }
