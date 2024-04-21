a7ca0f81cd7f0bf29b060adfe779077f1786226a
saraUsama9910 <sarausama80@gmail.com>
2024-04-21 22:04:13 +0200

    done state

--- lib/Features/home/peresntation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/peresntation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookapp/Features/home/data/models/book_model/book_model.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
--- lib/Features/home/peresntation/manager/featured_books_cubit/featured_books_state.dart
+++ lib/Features/home/peresntation/manager/featured_books_cubit/featured_books_state.dart
@@ -11,14 +11,12 @@ final class FeaturedBooksInitial extends FeaturedBooksState {}
 
 final class FeaturedBooksLoading extends FeaturedBooksState {}
 
-
 final class FeaturedBooksFailure extends FeaturedBooksState {
   final String errMessage;
-const FeaturedBooksFailure(this.errMessage);
-
+  const FeaturedBooksFailure(this.errMessage);
 }
 
 final class FeaturedBooksSuccess extends FeaturedBooksState {
- final List<BookModel>books;
- const FeaturedBooksSuccess(this.books);
+  final List<BookModel> books;
+  const FeaturedBooksSuccess(this.books);
 }
