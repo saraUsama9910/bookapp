efeac4083a5df2cb339a6836cb8bfd9d2cc39e89
saraUsama9910 <sarausama80@gmail.com>
2024-04-21 20:17:43 +0200

    retern right side

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -2,6 +2,7 @@ import 'package:bookapp/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookapp/Features/home/data/repos/home_repo.dart';
 import 'package:bookapp/core/errors/failures.dart';
 import 'package:bookapp/core/utils/api_service.dart';
+import 'package:dart_either/dart_either.dart';
 import 'package:dart_either/src/dart_either.dart';
 
 class HomeRepoImpl implements HomeRepo {
@@ -11,12 +12,17 @@ class HomeRepoImpl implements HomeRepo {
   @override
   Future<Either<Failure, List<BookModel>>> featchNewestBooks() async {
     try {
-  var date = await apiService.get(
-      endPoint:
-          'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:programming');
-} on Exception catch (e) {
-  // TODO
-}
+      var date = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:programming');
+      List<BookModel> books = [];
+      for (var item in date['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return Right(books);
+    } on Exception catch (e) {
+      // TODO
+    }
   }
 
   @override
