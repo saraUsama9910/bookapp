b456a65104d8a2a4326091477e615aed5a8ec998
saraUsama9910 <sarausama80@gmail.com>
2024-04-21 22:58:27 +0200

    create methods

--- lib/Features/home/peresntation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/peresntation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -8,4 +8,13 @@ part 'featured_books_state.dart';
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
   FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
   final HomeRepo homeRepo;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(ifLeft: (failure) {
+      emit(FeaturedBooksFailure(failure.errMessage));
+    }, ifRight: (books) {
+      emit(FeaturedBooksSuccess(books));
+    });
+  }
 }
