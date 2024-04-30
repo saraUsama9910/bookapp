aaccabb65aaf93707f27cb73728bf8bbe9ba07af
saraUsama9910 <sarausama80@gmail.com>
2024-04-30 10:46:58 +0300

    create custom loading indecator

--- lib/Features/home/peresntation/views/widgets/featured_books_list_view.dart
+++ lib/Features/home/peresntation/views/widgets/featured_books_list_view.dart
@@ -1,5 +1,7 @@
 import 'package:bookapp/Features/home/peresntation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookapp/Features/home/peresntation/views/widgets/custom_list_view_item.dart';
+import 'package:bookapp/core/widgets/custom_error_widget.dart';
+import 'package:bookapp/core/widgets/custom_loading_indicator.dart';
 import 'package:cubit_form/cubit_form.dart';
 import 'package:flutter/material.dart';
 
@@ -11,25 +13,27 @@ class FeaturedBooksListView extends StatelessWidget {
     return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
       builder: (context, state) {
         if (state is FeaturedBooksSuccess) {
-  return SizedBox(
-    height: MediaQuery.of(context).size.height * .25,
-    width: MediaQuery.of(context).size.height * 6,
-    child: ListView.builder(
-      scrollDirection: Axis.horizontal,
-      padding: EdgeInsets.zero,
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 4,
-          ),
-          child: FeaturedListViewItem(),
-        );
-      },
-    ),
-  );
-}else if(state is FeaturedBooksFailure){
-  
-}
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * .25,
+            width: MediaQuery.of(context).size.height * 6,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              padding: EdgeInsets.zero,
+              itemBuilder: (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 4,
+                  ),
+                  child: FeaturedListViewItem(),
+                );
+              },
+            ),
+          );
+        } else if (state is FeaturedBooksFailure) {
+          return CustomErrorWidget(errMessage: state.errMessage);
+        } else {
+          return const CustomLoadingIndicator();
+        }
       },
     );
   }
