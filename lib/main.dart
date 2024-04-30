d541ef1407887dfd75e06afe54bf13ff563979ae
saraUsama9910 <sarausama80@gmail.com>
2024-04-30 11:00:58 +0300

    .. fetch featured books in main for not using stateful widget for small action

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class BookApp extends StatelessWidget {
         BlocProvider(
           create: (context) => FeaturedBooksCubit(
             getIt.get<HomeRepoImpl>(),
-          ),
+          )..fetchFeaturedBooks(),
         ),
         BlocProvider(
           create: (context) => NewestBooksCubit(
